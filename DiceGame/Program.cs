4605cf3f7adbde636eb27e3bab16f31209d75272
Oros Cosmin-Cristian <98512765+cosmin-oros@users.noreply.github.com>
2022-05-29 00:25:17 +0300

    Add files via upload

---
+++ DiceGame/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Dice_Gane{
+    class Program{
+        static void Main(string[] args){
+            int playerRandomNum;
+            int enemyRandomNum;
+
+            int playerPoints = 0;
+            int enemyPoints = 0;
+
+            Random random = new Random();
+
+            for(int i=0; i<10;i++){
+                Console.WriteLine("Press any key to roll the dice.");
+
+                Console.ReadKey();
+
+                playerRandomNum = random.Next(1,7);
+                Console.WriteLine("You rolled a "+playerRandomNum);
+
+                Console.WriteLine("...");
+                System.Threading.Thread.Sleep(1000);
+
+                enemyRandomNum = random.Next(1,7);
+                Console.WriteLine("Opponent rolled a "+enemyRandomNum);
+
+                if(playerRandomNum > enemyRandomNum){
+                    playerPoints++;
+                    Console.Write("Player wins this round");
+                }else if(playerRandomNum < enemyRandomNum){
+                    enemyPoints++;
+                    Console.WriteLine("Opponent wins this round");
+                }else{
+                    Console.WriteLine("It's a tie");
+                }
+
+                Console.WriteLine("The score is now - Player : "+playerPoints+", Opponent: "+enemyPoints);
+                Console.WriteLine();
+            }
+            if(playerPoints > enemyPoints){
+                Console.WriteLine("You win!");
+            }else if(playerPoints < enemyPoints){
+                Console.WriteLine("You lose!");
+            }else{
+                Console.WriteLine("It's a tie");
+            }
+            Console.ReadKey();
+        }
+    }
+}
