f8799a7812476e1f462d52b6ef68c12d0a3e6301
cosmin-oros <oroscosmin09@gmail.com>
2022-06-05 10:53:59 +0300

    Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Calculator{
+    class Program{
+        static void Main(string[] args){
+            do{
+
+            double num1 = 0;
+            double num2 = 0;
+            double result = 0;
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("Calculator Program");
+            Console.WriteLine("------------------");
+
+            Console.Write("Enter number 1: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter number 2: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an option: ");
+            Console.WriteLine("\t+ : Add ");
+            Console.WriteLine("\t- : Subtract ");
+            Console.WriteLine("\t* : Multiply ");
+            Console.WriteLine("\t/ : Divide ");
+
+            Console.WriteLine("Enter an option: ");
+
+            switch(Console.ReadLine()){
+                case "+":
+                    result = num1 + num2;
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + result);
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + result);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+                Console.WriteLine("Would you like to continue? (Y or N)");
+            }while(Console.ReadLine().ToUpper() == "Y");
+
+            Console.ReadKey();
+        }
+    }
+}
